2249d96c1c126d8e13e4bbf7203a8b7dc8e263df
Charles <charles.dai@changefinancial.co.nz>
2021-09-13 13:54:05 +1200

    add result

--- testng.bat
+++ testng.bat
@@ -1,6 +1,10 @@
 @echo off
+if exist allure\allure-results （
 cd allure\allure-results
 for /r %%f in (*.*) do del %%f
 cd ..
 cd ..
-call mvn clean test -Dsurefire.suiteXmlFiles=src/main/resources/testng.xml 
+call mvn clean test -Dsurefire.suiteXmlFiles=src/main/resources/testng.xml
+）else(
+call mvn clean test -Dsurefire.suiteXmlFiles=src/main/resources/testng.xml
+)
