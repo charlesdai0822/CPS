84ec3b1e31ddbea8fe10478ed0fb17e38d3ba384
Charles <charles.dai@changefinancial.co.nz>
2021-09-13 20:44:45 +1200

    change to use testng.bat

--- testng.bat
+++ testng.bat
@@ -1,10 +1,10 @@
 @echo off
-if exist allure\allure-results （
+if exist allure\allure-results\ (
 cd allure\allure-results
 for /r %%f in (*.*) do del %%f
 cd ..
 cd ..
 call mvn clean test -Dsurefire.suiteXmlFiles=src/main/resources/testng.xml
-）else(
+) else (
 call mvn clean test -Dsurefire.suiteXmlFiles=src/main/resources/testng.xml
 )
